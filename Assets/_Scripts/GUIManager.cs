5ec5e228acefa6a05997a0e42b819e7a5c715a33
Bertuğ Benim <bertugbenim@gmail.com>
2017-03-25 01:14:07 +0200

    Main Menu Scene added Folders Organized TexturePacker Added GUI Functions added

---
+++ Assets/_Scripts/GUIManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GUIManager : MonoBehaviour {
+
+	public GameObject MainPanel;
+	public GameObject PlayerSelectionPanel;
+	private int selectedItemNumber;
+
+    public void OnPlayButton()
+    {
+		OpenGameScene ();
+		//ShowPlayerSelectionPanel ();
+    }
+	public void OnExitButton()
+	{
+		Application.Quit ();
+	}
+	public void BackButtonPressed()
+	{
+		ShowMainPanel ();
+	}
+	public void ShowMainPanel()
+	{
+		
+	}
+	private void ShowPlayerSelectionPanel()
+	{
+		
+	}
+	public static void OpenGameScene()
+	{
+		SceneManager.LoadScene ("GameScene");
+	}
+	public static void OpenMainMenu()
+	{
+		SceneManager.LoadScene ("MenuScene");
+	}
+}
