27960d963cc94a2cf08638707ffcf5d29eb65e7a
Bertuğ Benim <bertugbenim@gmail.com>
2017-03-25 02:50:34 +0200

    Blue and Brown Cupcake prefabs created Brown Cupcake now can be moved with the controller

---
+++ Assets/_Scripts/CupcakeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupcakeController : MonoBehaviour {
+	public float speed = 1f;
+	public float frictionConstant = .5f;
+	private Vector2 _velocity;
+	void Update () {
+		Vector2 _direction = GetJoystickValue ();
+		_velocity = _direction * speed;
+		if (_direction.x == 0f) {
+			_velocity.x *= 0.7f;
+		}
+		if (_direction.y == 0f) {
+			_velocity.y *= 0.7f;
+		}
+
+	}
+	void FixedUpdate() {
+		GetComponent<Rigidbody2D> ().velocity = _velocity; // Controlling Cupcake with stick
+	}
+	#region Helper Functions
+	private Vector2 GetJoystickValue() {
+		return new Vector2 (GetHorizontalValue(), GetVerticalValue());
+	}
+
+	private float GetVerticalValue() {
+		float r = 0f;
+		r += Input.GetAxis ("Vertical");
+		return Mathf.Clamp (r, -1.0f, 1.0f);
+	}
+
+	private float GetHorizontalValue() {
+		float r = 0f;
+		r += Input.GetAxis ("Horizontal");
+		return Mathf.Clamp (r, -1.0f, 1.0f);
+	}
+	#endregion
+}
---
+++ Assets/_Scripts/DepthSortController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Renderer))]
+public class DepthSortController : MonoBehaviour {
+
+	[Tooltip("Distance of object from the floor")]
+	public int HeightOffset = 0;
+
+	private const int IsometricRangePerYUnit = 100;
+
+	void Update()
+	{
+		SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+		renderer.sortingOrder = -(int)((transform.position.y - transform.localScale.y) * IsometricRangePerYUnit) + HeightOffset;
+	}
+}
